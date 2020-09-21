529f19dc9be938487770da110e872f3e26229023
tiagosn01 <tiagosilva0922@gmail.com>
2020-09-21 16:16:55 -0300

    Configurções de estilos

---
+++ src/styles/theme.js
@@ -0,0 +1,31 @@
+export default {
+  colors: {
+    primary: '#FF8700',
+    primaryDark: '#FF7A00',
+    secondary: '#FF500F',
+    grey: '#8C91A5',
+    greyLowerOpacity: '#abaebf',
+    lightGrey: '#F5F6FA',
+    darkGrey: '#6C757D',
+    success: '#20C05C',
+    error: '#FD4F48',
+    label: '#fff',
+    background: '#fff',
+    yellow: '#FFED00',
+  },
+  fontSizes: {
+    default: '1.4rem',
+    large: '1.6rem',
+    small: '1.2rem',
+    tiny: '1rem',
+  },
+  spacing: {
+    default: '1.6rem 1.6rem',
+    vertical: '1.6rem 0',
+    horizontal: '0 1.6rem',
+    large: '1.6rem 2.4rem',
+  },
+  transition: {
+    default: '180ms ease-in-out',
+  },
+};
