7c02d7d35baf7a5980ad2eea36de554d2723a892
tiagosn01 <tiagosilva0922@gmail.com>
2020-10-09 10:58:13 -0300

    Configuração HelpDesk

--- src/pages/Dashboard/SideBar/index.js
+++ src/pages/Dashboard/SideBar/index.js
@@ -3,20 +3,24 @@ import { FiBarChart2 } from 'react-icons/fi';
 import DropDown from './Dropdown';
 import { sidebarNavigation } from '../../../resources';
 
-import { Container, AmountInvested } from './styles';
+import { Container, AmountInvested, Wrapper } from './styles';
 import AccountBalance from './AccountBalance';
+import HelpDesk from './HelpDesk';
 
 const SideBar = () => {
   return (
-    <Container>
-      <AccountBalance />
-      <AmountInvested>
-        Ver Total Investido icone <FiBarChart2 size={16} />
-      </AmountInvested>
-      {Object.entries(sidebarNavigation).map(([key, value]) => (
-        <DropDown key={key} icon={value.icon} sectionName={key} />
-      ))}
-    </Container>
+    <Wrapper>
+      <Container>
+        <AccountBalance />
+        <AmountInvested>
+          Ver Total Investido icone <FiBarChart2 size={16} />
+        </AmountInvested>
+        {Object.entries(sidebarNavigation).map(([key, value]) => (
+          <DropDown key={key} icon={value.icon} sectionName={key} />
+        ))}
+      </Container>
+      <HelpDesk />
+    </Wrapper>
   );
 };
 
--- src/pages/Dashboard/SideBar/styles.js
+++ src/pages/Dashboard/SideBar/styles.js
@@ -3,10 +3,17 @@ import { transparentize } from 'polished';
 
 import Button from '../../../components/Button';
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 100%;
+  max-width: 26.4rem;
+`;
+
+export const Container = styled.section`
   ${({ theme }) => css`
     background: ${theme.colors.background};
-    max-width: 26.4rem;
+
     max-height: 60rem;
     width: 100%;
     height: 100%;
